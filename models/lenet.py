89c10041f077ea5d2bc8545e2f5b3cb1b49b7bb5
Adetunji Onafuwa <adetunjionafuwa@gmail.com>
2025-05-17 07:20:27 +0200

    feat: implement lenet using the original paper

--- models/lenet.py
+++ models/lenet.py
@@ -1,7 +1,8 @@
 """
 LeNet-5 model implementation in PyTorch, that was proposed by 'Yann LeCun et al. in 1998'.
 The original paper uses Tanh activation functions and average pooling layers.
-The model can be adaped using ReLU activation functions and max pooling layers."""
+The model can be adaped using ReLU activation functions and max pooling layers.
+"""
 
 import torch
 import torch.nn as nn
@@ -9,9 +10,9 @@ import torch.nn.functional as F
 
 
 class LeNet(nn.Module):
-    def __init__(self, num_classes:int=10) -> None:
+    def __init__(self, input_shape:int=1, num_classes:int=10) -> None:
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.conv1 = nn.Conv2d(in_channels=input_shape, out_channels=6, kernel_size=5)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
         self.fc1 = nn.Linear(120, 84)
@@ -37,8 +38,8 @@ def lenet(**kwargs) -> LeNet:
 
 if __name__ == "__main__":
     # Test the model
-    model = lenet(num_classes=10)
+    model = lenet(input_shape=1, num_classes=10)
 
-    x = torch.randn(1, 3, 32, 32)
+    x = torch.randn(1, 1, 32, 32)
     output = model(x)
     print(output) 
